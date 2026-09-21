7bf2262ac56e0c10d061630f77e53a375dc65307
TimmyAtom112 <167676288+TimmyAtom112@users.noreply.github.com>
2026-09-21 07:16:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,15 @@ const menuBtn = document.querySelector(".menu-btn");
 const nav = document.querySelector(".navbar nav");
 
 menuBtn.addEventListener("click", () => {
-  nav.classList.toggle("show");
+nav.classList.toggle("show");
+});
+
+/* Close mobile menu after clicking a link */
+
+const navLinks = document.querySelectorAll(".navbar nav a");
+
+navLinks.forEach(link => {
+link.addEventListener("click", () => {
+nav.classList.remove("show");
+});
 });
